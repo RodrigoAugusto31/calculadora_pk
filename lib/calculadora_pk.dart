d70ba70dfdfbb36b43a3a99bedf391d5528cfea5
RodrigoAugusto31 <r.auc@hotmail.com>
2023-07-01 12:11:42 -0300

    adding calculator

--- lib/calculadora_pk.dart
+++ lib/calculadora_pk.dart
@@ -1,7 +1,20 @@
 library calculadora_pk;
 
+import 'package:url_launcher/url_launcher.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+void abrirCalculadora() async {
+  const url = 'calculator:';
+  // ignore: deprecated_member_use
+  if (await canLaunch(url)) {
+    // ignore: deprecated_member_use
+    await launch(url);
+  } else {
+    throw 'Não foi possível abrir a calculadora.';
+  }
 }
