e9a4e3470e9984c9e134c85096cbb3f8c47f0252
RodrigoAugusto31 <r.auc@hotmail.com>
2023-07-01 12:54:53 -0300

    adding calculator 6.0

--- lib/calculadora_pk.dart
+++ lib/calculadora_pk.dart
@@ -1,16 +1,18 @@
 library calculadora_pk;
-
-import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 
-void openCalculator() {
-    // Chama o método para abrir a calculadora nativa do dispositivo
-    const platform = MethodChannel('calculadora_pk/openCalculator');
-    platform.invokeMethod('open');
+void openCalculator() async {
+  const url = 'calculator://'; // URL de exemplo, pode variar dependendo do dispositivo
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Não foi possível abrir a calculadora';
   }
+}
 }
 
